d94f4dd65215ac2f9cb58f6731236be2efdc501e
mel1bt <mberrocal.tipte@gmail.com>
2016-11-16 07:34:42 -0500

    actualizando css de index

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,23 @@
-/*var socket = io();
 
 $(document).ready(function() {
-	$("#login").submit(function(evt) {
-		evt.preventDefault();
-		var data = {
-			name: $(this).find("input").val(),
-			status: "Connecting..."
-		};
-		socket.emit("conectando", data);
-	});
-  $("#chat").submit(function(evt) {
-		evt.preventDefault();
-		var msj = {
-			texto: $(this).find("input").val(),
-			status: "Connecting..."
-		};
-		socket.emit("conectando", msj);
-	});
+
+	 $('body').vegas({
+        transitionDuration: 50000,
+        slides: [
+
+            {   src: 'img/video/fondo.jpg',
+            video: {
+                src: [
+                    'img/video/video5.mp4',
+                ],
+                loop: true,
+                mute: true
+            }
+        }
+        
+        ],
+    transition: 'fade',
+     overlay:  'dist/vegas/overlays/05.png'
+    });
 
 });
-
-socket.on("conectado", function (usuario) {
-	$("#usuarios").append(usuario.name + "<br>");
-  $("#chatUser").append(usuario.texto + "<br>");
-});*/
